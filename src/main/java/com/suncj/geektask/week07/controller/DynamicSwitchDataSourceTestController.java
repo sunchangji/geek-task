c8c84d02aac36a4c1deae9cf2d5b077aa8bc3775
孙长纪 <sunchangji@sunchangjideMacBook-Pro.local>
2021-08-08 00:01:19 +0800

    插入 100 万订单模拟数据，测试不同方式的插入效率

--- src/main/java/com/suncj/geektask/week07/controller/DynamicSwitchDataSourceTestController.java
+++ src/main/java/com/suncj/geektask/week07/controller/DynamicSwitchDataSourceTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -24,6 +25,36 @@ public class DynamicSwitchDataSourceTestController {
 
     @RequestMapping("create")
     public boolean create(){
+        TradeOrderDetailEntity entity = buildEntity(0);
+        tradeOrderDetailService.create(entity);
+        return true;
+    }
+
+    @RequestMapping("update")
+    public int create(String productName,long id){
+        return tradeOrderDetailService.update(productName,id);
+    }
+
+    /**
+     * 1w一次执行会爆Packet for query is too large (5544651 > 4194304),sql过长
+     * @return
+     */
+    @RequestMapping("batch100w")
+    public boolean create100wData(){
+
+        List<TradeOrderDetailEntity> list = new ArrayList<>(10000);
+        for (int i=0;i<1000000;i++){
+            list.add(buildEntity(i));
+            //1w插入一次
+            if(list.size() > 10000){
+                tradeOrderDetailService.batchCreate(list);
+                list = new ArrayList<>(10000);
+            }
+        }
+        return true;
+    }
+
+    private TradeOrderDetailEntity buildEntity(int i){
         TradeOrderDetailEntity entity = new TradeOrderDetailEntity();
         entity.setAverageCost(BigDecimal.valueOf(1.02));
         entity.setBuyNum(10);
@@ -36,12 +67,6 @@ public class DynamicSwitchDataSourceTestController {
         Date nowTime = new Date();
         entity.setCreateTime(nowTime);
         entity.setUpdateTime(nowTime);
-        tradeOrderDetailService.create(entity);
-        return true;
-    }
-
-    @RequestMapping("update")
-    public int create(String productName,long id){
-        return tradeOrderDetailService.update(productName,id);
+        return entity;
     }
 }
